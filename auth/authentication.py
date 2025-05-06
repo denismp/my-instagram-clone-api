969f24bec88c6e9ead5cbc4afb2bd4d278540ad5
Denis Putnam <denisputnam@verizon.net>
2025-05-06 14:49:53 -0400

    Reformatted .py files to be pep 8 compliant.

--- auth/authentication.py
+++ auth/authentication.py
@@ -6,25 +6,28 @@ from db.models import DbUser
 from db.hashing import Hash
 from auth.oauth2 import create_access_token
 
-router = APIRouter(
-  tags=['authentication']
-)
+router = APIRouter(tags=["authentication"])
 
-@router.post('/login')
-def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-  user = db.query(DbUser).filter(DbUser.username == request.username).first()
-  if not user:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-          detail='Invalid credentials')
-  if not Hash.verify(user.password, request.password):
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-          detail='Incorrect password')
-  
-  access_token = create_access_token(data={'username': user.username})
 
-  return {
-    'access_token': access_token,
-    'token_type': 'bearer',
-    'user_id': user.id,
-    'username': user.username
-  }
+@router.post("/login")
+def login(
+    request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = db.query(DbUser).filter(DbUser.username == request.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials"
+        )
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect password"
+        )
+
+    access_token = create_access_token(data={"username": user.username})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_id": user.id,
+        "username": user.username,
+    }
--- db/db_comment.py
+++ db/db_comment.py
@@ -5,18 +5,17 @@ from routers.schemas import CommentBase
 
 
 def create(db: Session, request: CommentBase):
-  new_comment = DbComment(
-    text = request.text,
-    username = request.username,
-    post_id = request.post_id,
-    timestamp = datetime.now()
-  )
-  db.add(new_comment)
-  db.commit()
-  db.refresh(new_comment)
-  return new_comment
-
+    new_comment = DbComment(
+        text=request.text,
+        username=request.username,
+        post_id=request.post_id,
+        timestamp=datetime.now(),
+    )
+    db.add(new_comment)
+    db.commit()
+    db.refresh(new_comment)
+    return new_comment
 
 
 def get_all(db: Session, post_id: int):
-  return db.query(DbComment).filter(DbComment.post_id == post_id).all()
+    return db.query(DbComment).filter(DbComment.post_id == post_id).all()
